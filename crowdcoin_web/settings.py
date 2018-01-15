082b4bf8dc87985ece7795a6d8fd1667d116db0c
kea <kea@crowdcoin.co.za>
2018-01-15 17:17:43 +0200

    Added Sessions app

--- crowdcoin_web/settings.py
+++ crowdcoin_web/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.sites',
+    'django.contrib.sessions',
     'www',
     'payfast',
     'freshdesk'
