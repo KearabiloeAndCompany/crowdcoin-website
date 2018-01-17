08042715386b76e34dde0d955d7909b39fe77b4b
kea <kea@crowdcoin.co.za>
2018-01-17 17:06:24 +0200

    [UPDATE] Allowed hosts

--- crowdcoin_web/settings.py
+++ crowdcoin_web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG',False)
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(',') if os.environ.get('DJANGO_ALLOWED_HOSTS') else ['*']
 
 CROWDCOIN_API_URL =  os.environ.get('CROWDCOIN_API_URL')
 
